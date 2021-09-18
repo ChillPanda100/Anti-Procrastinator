7d1ebde7b9beff160222f15a43d5f893b79de306
ChillPanda100 <80004490+ChillPanda100@users.noreply.github.com>
2021-09-18 17:49:33 -0400

    Added more commands

--- Cogs/help.py
+++ Cogs/help.py
@@ -7,7 +7,7 @@ class events(commands.Cog):
 	
 	@commands.command()
 	async def help(self, ctx):
-		embed_var = Embed.embed("Help Section", "``#mute``: Mute yourself for a certain amount of time. Example: #mute 4:52 PM\n ``#timezones``: Get a list of all the valid timezones.\n ``#setrole``: Set a role for the server (Admin only)\n``#credits``: Displays the credits and everyone involved.")
+		embed_var = Embed.embed("Help Section", "``#mute``: Mute yourself for a certain amount of time. Example: #mute 4:52 PM\n ``#instructions``: Learn how to use the bot.\n ``#timezones``: Get a list of all the valid timezones.\n ``#setrole``: Set a role for the server (Admin only).\n ``#github``: Link to GitHub repository.\n ``#credits``: Displays the credits and everyone involved.")
 		embed_var.set_footer(text="Made with Discord.py")
 		await ctx.send(embed=embed_var)
 	
@@ -19,7 +19,12 @@ class events(commands.Cog):
 	
 	@commands.command()
 	async def instructions(self, ctx):
-		embed_var = Embed.embed("Instructions" "``For admins``: Use the ``#setrole`` command to set a role (Make sure the role is isolated to a channel) that will be given to the users when muted. \n ``For users``: Use the ``#mute`` command and enter the time you want to be muted, followed by ``AM/PM`` and the timezone of your choice (Use #timezones to get a list of all the available timezones).")
+		embed_var = Embed.embed("Instructions", "``For admins``: Use the ``#setrole`` command to set a role (Make sure the role is isolated to a channel) that will be given to the users when muted. \n\n ``For users``: Use the ``#mute`` command and enter the time you want to be muted, followed by ``AM/PM`` and the timezone of your choice (Use #timezones to get a list of all the available timezones).")
+		await ctx.send(embed=embed_var)
+	
+	@commands.command()
+	async def github(self, ctx):
+		embed_var = Embed.embed("GitHub Repository", "``Link``: https://github.com/ChillPanda100/Anti-Procrastinator")
 		await ctx.send(embed=embed_var)
 	
 def setup(client):
