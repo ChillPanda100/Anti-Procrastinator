e122a087331ddfe9e8e340c5719ce58edfbfd71e
ChillPanda100 <80004490+ChillPanda100@users.noreply.github.com>
2021-09-18 17:26:47 -0400

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+
+bot = Flask("")
+
+@bot.route('/')
+def home():
+	return "Bot is online"
+
+def run():
+	bot.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+	thread = Thread(target=run)
+	thread.run()
