e31f19b0747c791e99fb879083a34265e0c59c17
ChillPanda100 <80004490+ChillPanda100@users.noreply.github.com>
2021-09-26 18:59:09 -0400

    Update help.py
    Credits will now adapt to name changes by the developers.

--- Cogs/help.py
+++ Cogs/help.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from main import *
 import Embed
 
 class events(commands.Cog):
@@ -7,19 +8,22 @@ class events(commands.Cog):
 	
 	@commands.command()
 	async def help(self, ctx):
-		embed_var = Embed.embed("Help Section", "``#mute``: Mute yourself for a certain amount of time. Example: #mute 4:52 PM\n ``#instructions``: Learn how to use the bot.\n ``#timezones``: Get a list of all the valid timezones.\n ``#setrole``: Set a role for the server (Admin only).\n ``#github``: Link to GitHub repository.\n ``#credits``: Displays the credits and everyone involved.")
+		embed_var = Embed.embed("Help Section", "``#mute``: Mute yourself for a certain amount of time. Example: #mute 4:52 PM EST\n ``#instructions``: Learn how to use the bot.\n ``#timezones``: Get a list of all the valid timezones.\n ``#setrole``: Set a role for the server (Admin only).\n ``#github``: Link to GitHub repository.\n ``#credits``: Displays the credits and everyone involved.")
 		embed_var.set_footer(text="Made with Discord.py")
 		await ctx.send(embed=embed_var)
 	
 	@commands.command()
 	async def credits(self, ctx):
-		embed_var = Embed.embed("Credits", "Anti-Procrastinator was created by ShyguyIV#6970, fisherman#2033, and ChillPanda#5842")
+		developer_one = await client.fetch_user(702690873024053309)
+		developer_two = await client.fetch_user(882795835371450499)
+		developer_three = await client.fetch_user(771153822994530354)
+		embed_var = Embed.embed("Credits", f"Anti-Procrastinator was created by {developer_one}, {developer_two}, and {developer_three}")
 		embed_var.set_footer(text="Made with Discord.py")
 		await ctx.send(embed=embed_var)
 	
 	@commands.command()
 	async def instructions(self, ctx):
-		embed_var = Embed.embed("Instructions", "``For admins``: Use the ``#setrole`` command to set a role (Make sure the role is isolated to a channel) that will be given to the users when muted. \n\n ``For users``: Use the ``#mute`` command and enter the time you want to be muted, followed by ``AM/PM`` and the timezone of your choice (Use #timezones to get a list of all the available timezones).")
+		embed_var = Embed.embed("Instructions", "``For admins``: Use the ``#setrole`` command to set a role (Make sure the role is isolated to a channel) that will be given to the users when muted. Additionally, move the \"Anti-Procrastinator\" role to the very top of the role hierarchy if possible. \n\n ``For users``: Use the ``#mute`` command and enter the time you want to be muted, followed by ``AM/PM`` and the timezone of your choice (Use #timezones to get a list of all the available timezones).")
 		await ctx.send(embed=embed_var)
 	
 	@commands.command()
